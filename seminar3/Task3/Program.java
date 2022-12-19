2ef0c7a6d0d250316b5d24e9609aad13e4a37ea8
G0ryny4 <gorynych90@gmail.com>
2022-12-19 08:55:13 +0300

    added  tasks from seminar 3

---
+++ seminar3/Task3/Program.java
@@ -0,0 +1,56 @@
+// Задан целочисленный список ArrayList. 
+// Найти минимальное, максимальное и среднее из этого списка.
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println(
+                "Введите ряд чисел. Вводить строго через один пробел.");
+        List<String> strList = new ArrayList<String>(
+                Arrays.asList(iScanner.nextLine().split(" ")));
+        iScanner.close();
+        System.out.printf("Заданный список: %s \n", String.join(", ", strList));
+        CorrectSymbols(strList);
+        int min = MixInt(strList);
+        int max = MaxInt(strList);
+        double average = Average(strList);
+        System.out.printf("В списке минимальное число -%d, максимальное - %d, среднее - %f.2", min, max, average);
+    }
+
+    public static int MixInt(List<String> listNumbers) {
+        int min = Integer.parseInt(listNumbers.get(0));
+        for (int i = 0; i < listNumbers.size(); i++) {
+            if (Integer.parseInt(listNumbers.get(i)) < min)
+                min = Integer.parseInt(listNumbers.get(i));
+        }
+        return min;
+    }
+
+    public static int MaxInt(List<String> listNumbes) {
+        int max = Integer.parseInt(listNumbes.get(0));
+        for (int i = 0; i < listNumbes.size(); i++) {
+            if (Integer.parseInt(listNumbes.get(i)) > max)
+                max = Integer.parseInt(listNumbes.get(i));
+        }
+        return max;
+    }
+
+    public static double Average(List<String> listNumbers) {
+        double sum = 0;
+        for (int i = 0; i < listNumbers.size(); i++) {
+            sum += Double.parseDouble(listNumbers.get(i));
+        }
+        double average = sum / listNumbers.size();
+        return average;
+    }
+
+    public static void CorrectSymbols(List<String> strList) {
+        strList.removeAll(Arrays.asList(null, "", null));
+        strList.removeAll(Arrays.asList(",", "", null));
+    }
+}
